601edc1038ad59c1b056f03303e5a2ba14b8d66b
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-02-19 19:48:27 -0600

    serving index.html

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const app = express();
 app.listen(3000, () => {
 	console.log('listening at PORT: 3000');
 });
+// display whats in public folder, the only file index.html
+app.use(express.static('public'));
