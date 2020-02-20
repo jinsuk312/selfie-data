b4863a234b493421e4d579b12683cb502cfd7f42
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-02-19 20:27:40 -0600

    added location to db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => {
 	console.log('listening at PORT: 3000');
@@ -6,13 +7,19 @@ app.listen(3000, () => {
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 //routes
 app.post('/api', (request, response) => {
-	console.log(request.body);
 	const data = request.body;
+	const timestamp = Date.now();
+	data.timestamp = timestamp;
+	database.insert(data);
 	response.json({
 		status: 'success',
 		latitude: data.lat,
+		timestamp: timestamp,
 		longitude: data.lon
 	});
 });
