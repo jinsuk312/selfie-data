18fcb0503fe7f1a8a0f5f1d05db3dcdb1ab225b6
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-02-19 20:20:32 -0600

    displaying and grab users current location via geolocation

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-// call dependency
 const express = require('express');
-// init express
 const app = express();
 app.listen(3000, () => {
 	console.log('listening at PORT: 3000');
 });
-// display whats in public folder, the only file index.html
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+//routes
+app.post('/api', (request, response) => {
+	console.log(request.body);
+	const data = request.body;
+	response.json({
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.lon
+	});
+});
